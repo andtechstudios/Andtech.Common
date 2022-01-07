fdc90b821b5ba236931669520959ee0a11ae387c
Andrew <andrewmjordan@users.noreply.github.com>
2022-01-07 15:41:46 -0800

    add error stream

--- src/Andtech.Common/Log.cs
+++ src/Andtech.Common/Log.cs
@@ -30,5 +30,33 @@ namespace Andtech.Common
 				Console.ForegroundColor = temp;
 			}
 		}
+
+		public static class Error
+		{
+
+			/// <summary>
+			/// Writes the text representation of <paramref name="message"/> to STDERR if <paramref name="verbosity"/> is enabled.
+			/// </summary>
+			/// <param name="message">The message to print.</param>
+			/// <param name="verbosity">The minimum verbosity required.</param>
+			public static void WriteLine(object message, Verbosity verbosity = default) => WriteLine(message, Console.ForegroundColor, verbosity);
+
+			/// <summary>
+			/// Writes the text representation of <paramref name="message"/> to STDERR if <paramref name="verbosity"/> is enabled.
+			/// </summary>
+			/// <param name="message">The message to print.</param>
+			/// <param name="foregroundColor">The color of the foreground.</param>
+			/// <param name="verbosity">The minimum verbosity required.</param>
+			public static void WriteLine(object message, ConsoleColor foregroundColor, Verbosity verbosity = default)
+			{
+				if ((int)verbosity <= (int)Verbosity)
+				{
+					var temp = Console.ForegroundColor;
+					Console.ForegroundColor = foregroundColor;
+					Console.Error.WriteLine(message);
+					Console.ForegroundColor = temp;
+				}
+			}
+		}
 	}
 }
