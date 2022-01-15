355bfe4f585432facf4a8e64896f3017aee4c172
Andrew <andrewmjordan@users.noreply.github.com>
2022-01-15 14:22:10 -0800

    add verbosity

--- src/Andtech.Common/Verbosity.cs
+++ src/Andtech.Common/Verbosity.cs
@@ -4,10 +4,11 @@ namespace Andtech.Common
 
 	public enum Verbosity
 	{
-		quiet = -2,
-		minimal = -1,
+		quiet = -20,
+		minimal = -10,
 		normal = 0,
-		verbose = 1,
-		silly = 2,
+		verbose = 10,
+		diagnostic = 20,
+		silly = 30,
 	}
 }
