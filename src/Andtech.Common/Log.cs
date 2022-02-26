7d9ad618ceabbef9be4611944df7c7fe1594fb8b
Andrew <andrewmjordan@users.noreply.github.com>
2022-02-26 11:10:25 -0800

    update "Log.cs"

--- src/Andtech.Common/Log.cs
+++ src/Andtech.Common/Log.cs
@@ -1,74 +1,112 @@
 ﻿using System;
+using System.IO;
 
 namespace Andtech.Common
 {
 
-	public static class Log
+	public class LogWriter
 	{
-		public static Verbosity Verbosity { get; set; } = default;
+		internal Verbosity Verbosity { get; set; }
+
+		private readonly TextWriter writer;
+
+		public LogWriter(TextWriter writer)
+		{
+			this.writer = writer;
+		}
 
 		/// <summary>
-		/// Writes a blank line if <paramref name="verbosity"/> is enabled.
+		/// Writes a blank line if <paramref name="verbosity"/> is allowed.
 		/// </summary>
 		/// <param name="verbosity">The minimum verbosity required.</param>
-		public static void WriteLine(Verbosity verbosity = default) => WriteLine(string.Empty, Console.ForegroundColor, verbosity);
+		public void Write(Verbosity verbosity = default) => Write(string.Empty, Console.ForegroundColor, verbosity);
 
 		/// <summary>
-		/// Writes the text representation of <paramref name="message"/> if <paramref name="verbosity"/> is enabled.
+		/// Writes the text representation of <paramref name="message"/> if <paramref name="verbosity"/> is allowed.
 		/// </summary>
 		/// <param name="message">The message to print.</param>
 		/// <param name="verbosity">The minimum verbosity required.</param>
-		public static void WriteLine(object message, Verbosity verbosity = default) => WriteLine(message, Console.ForegroundColor, verbosity);
+		public void Write(object message, Verbosity verbosity = default) => Write(message, Console.ForegroundColor, verbosity);
 
 		/// <summary>
-		/// Writes the text representation of <paramref name="message"/> if <paramref name="verbosity"/> is enabled.
+		/// Writes the text representation of <paramref name="message"/> if <paramref name="verbosity"/> is allowed.
 		/// </summary>
 		/// <param name="message">The message to print.</param>
 		/// <param name="foregroundColor">The color of the foreground.</param>
 		/// <param name="verbosity">The minimum verbosity required.</param>
-		public static void WriteLine(object message, ConsoleColor foregroundColor, Verbosity verbosity = default)
+		public void Write(object message, ConsoleColor foregroundColor, Verbosity verbosity = default)
 		{
 			if ((int)verbosity <= (int)Verbosity)
 			{
 				var temp = Console.ForegroundColor;
 				Console.ForegroundColor = foregroundColor;
-				Console.WriteLine(message);
+				writer.Write(message);
 				Console.ForegroundColor = temp;
 			}
 		}
 
-		public static class Error
+		/// <summary>
+		/// Writes a blank line followed by line termination, if <paramref name="verbosity"/> is allowed.
+		/// </summary>
+		/// <param name="verbosity">The minimum verbosity required.</param>
+		public void WriteLine(Verbosity verbosity = default) => WriteLine(string.Empty, Console.ForegroundColor, verbosity);
+
+		/// <summary>
+		/// Writes the text representation of <paramref name="message"/> followed by line termination, if <paramref name="verbosity"/> is allowed.
+		/// </summary>
+		/// <param name="message">The message to print.</param>
+		/// <param name="verbosity">The minimum verbosity required.</param>
+		public void WriteLine(object message, Verbosity verbosity = default) => WriteLine(message, Console.ForegroundColor, verbosity);
+
+		/// <summary>
+		/// Writes the text representation of <paramref name="message"/> followed by line termination, if <paramref name="verbosity"/> is allowed.
+		/// </summary>
+		/// <param name="message">The message to print.</param>
+		/// <param name="foregroundColor">The color of the foreground.</param>
+		/// <param name="verbosity">The minimum verbosity required.</param>
+		public void WriteLine(object message, ConsoleColor foregroundColor, Verbosity verbosity = default)
 		{
-
-			/// <summary>
-			/// Writes a blank line to STDERR if <paramref name="verbosity"/> is enabled.
-			/// </summary>
-			/// <param name="verbosity">The minimum verbosity required.</param>
-			public static void WriteLine(Verbosity verbosity = default) => WriteLine(string.Empty, Console.ForegroundColor, verbosity);
-
-			/// <summary>
-			/// Writes the text representation of <paramref name="message"/> to STDERR if <paramref name="verbosity"/> is enabled.
-			/// </summary>
-			/// <param name="message">The message to print.</param>
-			/// <param name="verbosity">The minimum verbosity required.</param>
-			public static void WriteLine(object message, Verbosity verbosity = default) => WriteLine(message, Console.ForegroundColor, verbosity);
-
-			/// <summary>
-			/// Writes the text representation of <paramref name="message"/> to STDERR if <paramref name="verbosity"/> is enabled.
-			/// </summary>
-			/// <param name="message">The message to print.</param>
-			/// <param name="foregroundColor">The color of the foreground.</param>
-			/// <param name="verbosity">The minimum verbosity required.</param>
-			public static void WriteLine(object message, ConsoleColor foregroundColor, Verbosity verbosity = default)
+			if ((int)verbosity <= (int)Verbosity)
 			{
-				if ((int)verbosity <= (int)Verbosity)
-				{
-					var temp = Console.ForegroundColor;
-					Console.ForegroundColor = foregroundColor;
-					Console.Error.WriteLine(message);
-					Console.ForegroundColor = temp;
-				}
+				var temp = Console.ForegroundColor;
+				Console.ForegroundColor = foregroundColor;
+				writer.WriteLine(message);
+				Console.ForegroundColor = temp;
 			}
 		}
+	}
+
+	public static class Log
+	{
+		public static Verbosity Verbosity
+		{
+			get => Out.Verbosity;
+			set
+			{
+				Out.Verbosity = value;
+				Error.Verbosity = value;
+			}
+		}
+
+		public static LogWriter Out { get; private set; } = new LogWriter(Console.Out);
+		public static LogWriter Error { get; private set; } = new LogWriter(Console.Error);
+
+		/// <inheritdoc cref="LogWriter.Write(Verbosity)" />
+		public static void Write(Verbosity verbosity = default) => Write(string.Empty, Console.ForegroundColor, verbosity);
+
+		/// <inheritdoc cref="LogWriter.Write(object, Verbosity)" />
+		public static void Write(object message, Verbosity verbosity = default) => Write(message, Console.ForegroundColor, verbosity);
+
+		/// <inheritdoc cref="LogWriter.Write(object, ConsoleColor, Verbosity)" />
+		public static void Write(object message, ConsoleColor foregroundColor, Verbosity verbosity = default) => Out.Write(message, foregroundColor, verbosity);
+
+		/// <inheritdoc cref="LogWriter.WriteLine(Verbosity)" />
+		public static void WriteLine(Verbosity verbosity = default) => WriteLine(string.Empty, Console.ForegroundColor, verbosity);
+
+		/// <inheritdoc cref="LogWriter.WriteLine(object, Verbosity)" />
+		public static void WriteLine(object message, Verbosity verbosity = default) => WriteLine(message, Console.ForegroundColor, verbosity);
+
+		/// <inheritdoc cref="LogWriter.WriteLine(object, ConsoleColor, Verbosity)" />
+		public static void WriteLine(object message, ConsoleColor foregroundColor, Verbosity verbosity = default) => Out.WriteLine(message, foregroundColor, verbosity);
 	}
 }
