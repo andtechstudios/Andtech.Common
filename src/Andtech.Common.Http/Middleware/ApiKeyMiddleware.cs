c4e378deadb52a7e6c78bf23b7d7422920a62eab
Andrew <andrewmjordan@users.noreply.github.com>
2022-05-02 03:12:45 -0700

    add http files

---
+++ src/Andtech.Common.Http/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Andtech.Common.Http
+{
+
+	public class ApiKeyMiddleware
+	{
+		public static Func<string, bool> TestApiKey { get; set; }
+
+		private readonly RequestDelegate _next;
+
+		public ApiKeyMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			if (TestApiKey != null)
+			{
+				if (!context.Request.Headers.TryGetValue("ApiKey", out var requestApiKey))
+				{
+					context.Response.StatusCode = 401;
+					await context.Response.WriteAsync("UNAUTHORIZED");
+					return;
+				}
+
+				if (!TestApiKey(requestApiKey))
+				{
+					context.Response.StatusCode = 401;
+					await context.Response.WriteAsync("UNAUTHORIZED");
+					return;
+				}
+			}
+
+			await _next(context);
+		}
+	}
+}
